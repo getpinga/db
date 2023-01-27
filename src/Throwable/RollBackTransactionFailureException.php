c73ffb4a95998cb0eefa8550b81251c4f7f61b6f
Pinga <121483313+getpinga@users.noreply.github.com>
2023-01-27 17:14:16 +0200

    Update RollBackTransactionFailureException.php

--- src/Throwable/RollBackTransactionFailureException.php
+++ src/Throwable/RollBackTransactionFailureException.php
@@ -1,12 +1,15 @@
 <?php
 
-/*
- * PHP-DB (https://github.com/delight-im/PHP-DB)
- * Copyright (c) delight.im (https://www.delight.im/)
- * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+/**
+ * Pinga DB
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ * Based on PHP-DB (https://github.com/delight-im/PHP-DB) by delight.im (https://www.delight.im/)
+ *
+ * @license MIT
  */
 
-namespace Delight\Db\Throwable;
+namespace Pinga\Db\Throwable;
 
 /** Exception that is thrown when a transaction cannot be rolled back successfully for some reason */
 class RollBackTransactionFailureException extends TransactionFailureException {}
